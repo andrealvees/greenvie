7d1fde50ebe88a1c9045ee42ffe6c50529efa557
André Alves <91576203+andrealvees@users.noreply.github.com>
2023-07-31 19:56:33 +0000

    Update 2 files

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="style.css">
+</head>
+<body>
+        <header>
+                <a href="/index.html" id="brand">Titulo do Site</a>
+                <form action="/" method="post">
+                        <input type="text" name="search" id="filme_search" placeholder="Buscar filme">
+                        <input type="submit" value="X">
+                </form>
+        </header>
+        <main>
+                <div class="lateral-menu">
+                        <ul>
+                                <a href="" class="menu-link"><li class="menu-item">Inicio</li></a>
+                                <a href="" class="menu-link"><li class="menu-item">Avaliações</li></a>
+                                <a href="" class="menu-link"><li class="menu-item">Ranking</li></a>
+                                <a href="" class="menu-link"><li class="menu-item">Configurações</li></a>
+                        </ul>
+                </div>
+                <div class="content"></div>
+        </main>
+</body>
+</html>

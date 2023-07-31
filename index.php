d9c5ab18da10a7075d4f866f16b667dd986aaa2d
André Alves <91576203+andrealvees@users.noreply.github.com>
2023-07-31 11:41:04 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
+<?php
+    echo "teste";
+?>
 
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>GreenVie</title>
+    </head>
+    <body>
+        <h1>Teste</h1>
+    </body>
+</html>
